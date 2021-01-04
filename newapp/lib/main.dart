e7967a780471e5b0a3b81664050f84805ad127a2
Vinay Sudrik <vinaysudrik@gmail.com>
2021-01-04 20:09:20 +0530

    first commit

---
+++ newapp/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Awesome App")),
+      body: Container(child: Text("Hi Flutter")),
+    );
+  }
+}
