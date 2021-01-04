cfa32f550125944e101e7b687913158d4a9b0878
Vinay Sudrik <vinaysudrik@gmail.com>
2021-01-04 21:26:37 +0530

    Update main.dart

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -13,8 +13,67 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Awesome App")),
-      body: Container(child: Text("Hi Flutter")),
-    );
+        appBar: AppBar(title: Text("Awesome App")),
+        body: Container(
+            width: 500,
+            color: Colors.teal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 100,
+                  width: 100,
+                  clipBehavior: Clip.antiAlias,
+                  alignment: Alignment.topLeft,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                      gradient:
+                          LinearGradient(colors: [Colors.pink, Colors.yellow]),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 21,
+                        )
+                      ]),
+                  child: Text(
+                    "I am a box",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 100,
+                  width: 100,
+                  clipBehavior: Clip.antiAlias,
+                  alignment: Alignment.topLeft,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                      gradient:
+                          LinearGradient(colors: [Colors.blue, Colors.yellow]),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 21,
+                        )
+                      ]),
+                  child: Text(
+                    "I am a box",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            )));
   }
 }
